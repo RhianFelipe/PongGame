02b16ccbb723c56d7e91972284eca474a461b40a
Rhian Felipe Silva de Lara <62002116+RhianFelipe@users.noreply.github.com>
2021-04-06 11:39:34 -0300

    Add files via upload

---
+++ Assets/Scripts/BolaController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BolaController : MonoBehaviour
+{
+    
+    //Criando a variavel para saber quem é o meu RigidBody
+    public Rigidbody2D meuRB;
+    private Vector2 minhaVelocidade;
+    public float velocidade = 5f; 
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Passando a minha velocidade para minhaVelocidade
+        minhaVelocidade.x = -velocidade;
+        //Adicionando uma velocidade para a esquerda
+        meuRB.velocity = minhaVelocidade;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RaqueteControler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaqueteControler : MonoBehaviour
+{
+
+    private Vector3 minhaPosicao; 
+    public float meuY;
+    public float velocidade = 5f;
+    public float meuLimite = 3.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //pegando a posição inicial da raquete e aplicando à minha posição
+        minhaPosicao = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+        //Passando o meuY para o eixo Y da minhaPosicao
+        minhaPosicao.y = meuY;
+
+        //modificar a posicao da minha raquete
+        transform.position = minhaPosicao;
+
+        //Pegando a setinha para cima
+        //Se eu apertar a setinha para cima ele vai subir a raquete
+        if (Input.GetKey(KeyCode.UpArrow)) {//primeira forma
+            //Checar se meuY é menor do que o meu limite
+            if(meuY < meuLimite)
+            {
+                //Ação para quando a setinha para cima for pressionada
+                //Aumentar o valor do meuY APENAS SE ele for menor do que o meu limite
+                meuY = meuY + velocidade * Time.deltaTime; //Para se igualar a qualquer PC
+            }
+
+           
+
+        }
+        //Se eu apertar a setinha para baixo ele vai descer a raquete E Aumentar o valor do meuY APENAS SE ele for maior do que o meu limite
+        if (Input.GetKey(KeyCode.DownArrow) && meuY > -meuLimite) //segundo forma
+        {
+     
+                //Ação para quando a setinha para baixo for pressionada
+                //Aumentar o valor do meuY APENAS SE ele for maior do que o meu limite
+                meuY = meuY - velocidade * Time.deltaTime; //Para se igualar a qualquer PC
+           
+        }
+    }
+}
